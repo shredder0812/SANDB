17ecd9a3bc6c08605b191d17f6e522d40bb304e9
shredder0812 <119552922+shredder0812@users.noreply.github.com>
2023-06-09 11:35:28 +0700

    Add files via upload

---
+++ 2testlienket.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const express = require('express');
+
+const test1 = require('./1testlienket');
+console.log(test1.insertCircle)
+console.log(test1.insertLine)
+console.log(test1.insertPolyline)
